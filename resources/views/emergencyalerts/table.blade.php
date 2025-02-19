240856cfae5dcee08024a674b8bd1e06bf0a1ce9
Komal <B00139870@mytudublin.ie>
2025-02-19 22:13:04 +0000

    Updated emergency alerts to show resident names instead of IDs

--- resources/views/emergencyalerts/table.blade.php
+++ resources/views/emergencyalerts/table.blade.php
@@ -1,28 +1,28 @@
 <table class="table table-responsive" id="emergencyalerts-table">
     <thead>
-        <th>Residentid</th>
-        <th>Triggeredbyid</th>
-        <th>Alerttype</th>
-        <th>Alerttimestamp</th>
+        <th>Resident Name</th> <!-- Changed from Resident ID -->
+        <th>Triggered By</th>
+        <th>Alert Type</th>
+        <th>Alert Timestamp</th>
         <th>Status</th>
-        <th>Resolvedbyid</th>
+        <th>Resolved By</th>
         <th colspan="3">Action</th>
     </thead>
     <tbody>
     @foreach($emergencyalerts as $emergencyalert)
         <tr>
-            <td>{!! $emergencyalert->residentid !!}</td>
-            <td>{!! $emergencyalert->triggeredbyid !!}</td>
+            <td>{!! $emergencyalert->resident->name ?? 'Unknown' !!}</td> <!-- Display Resident Name -->
+            <td>{!! $emergencyalert->triggeredBy->name ?? 'Unknown' !!}</td> <!-- If available -->
             <td>{!! $emergencyalert->alerttype !!}</td>
             <td>{!! $emergencyalert->alerttimestamp !!}</td>
             <td>{!! $emergencyalert->status !!}</td>
-            <td>{!! $emergencyalert->resolvedbyid !!}</td>
+            <td>{!! $emergencyalert->resolvedBy->name ?? 'N/A' !!}</td> <!-- If available -->
             <td>
                 {!! Form::open(['route' => ['emergencyalerts.destroy', $emergencyalert->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
-                    <a href="{!! route('emergencyalerts.show', [$emergencyalert->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-eye"></i></i></a>
-                    <a href="{!! route('emergencyalerts.edit', [$emergencyalert->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-edit"></i></i></a>
-                    {!! Form::button('<i class="far fa-trash-alt"></i></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                    <a href="{!! route('emergencyalerts.show', [$emergencyalert->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-eye"></i></a>
+                    <a href="{!! route('emergencyalerts.edit', [$emergencyalert->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-edit"></i></a>
+                    {!! Form::button('<i class="far fa-trash-alt"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                 </div>
                 {!! Form::close() !!}
             </td>
