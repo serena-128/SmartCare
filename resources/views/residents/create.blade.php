783f6ec3b1ffbe4cad47e312b3f595515e15c310
Komal <B00139870@mytudublin.ie>
2025-02-24 19:13:07 +0000

    doctors note added

--- resources/views/residents/create.blade.php
+++ resources/views/residents/create.blade.php
@@ -21,4 +21,24 @@
             </div>
         </div>
     </div>
+<div class="form-group">
+    <label for="medical_history">Medical History:</label>
+    <textarea name="medical_history" class="form-control">{{ old('medical_history', $resident->medical_history ?? '') }}</textarea>
+</div>
+
+<div class="form-group">
+    <label for="allergies">Allergies:</label>
+    <textarea name="allergies" class="form-control">{{ old('allergies', $resident->allergies ?? '') }}</textarea>
+</div>
+
+<div class="form-group">
+    <label for="medications">Medications:</label>
+    <textarea name="medications" class="form-control">{{ old('medications', $resident->medications ?? '') }}</textarea>
+</div>
+
+<div class="form-group">
+    <label for="doctor_notes">Doctor's Notes:</label>
+    <textarea name="doctor_notes" class="form-control">{{ old('doctor_notes', $resident->doctor_notes ?? '') }}</textarea>
+</div>
+
 @endsection
