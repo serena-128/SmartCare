cb181e628552edad4faf22b699dcd42db6684acf
Komal <B00139870@mytudublin.ie>
2025-03-14 01:52:18 +0000

    app updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,48 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>{{ config('app.name') }}</title>
-        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-              integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-              crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <!-- Bootstrap 5 -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    </head>
-    <body>
-        <!-- Bootstrap 5 Navbar -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>{{ config('app.name') }}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-        <div id="page-content-wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                    <!-- Well for left side of the page (to compensate for widescreen) -->
-                    <div class="col-lg-2"></div>
-                    <!-- Slot for content from Lower Level views with sections called 'content' pulled in here -->
-                    <div class="col-lg-8"> @yield('content') </div>
-                    <!-- Well for right side of the page (to compensate for widescreen) -->
-                    <div class="col-lg-2"></div>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+          integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+    <!-- Bootstrap 5 Navbar (Add if needed) -->
+
+    <div id="page-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- Left Side Space (for widescreen adjustment) -->
+                <div class="col-lg-2"></div>
+
+                <!-- Main Content -->
+                <div class="col-lg-8"> 
+                    @yield('content') 
                 </div>
+
+                <!-- Right Side Space (for widescreen adjustment) -->
+                <div class="col-lg-2"></div>
             </div>
         </div>
-        <!-- Webpack mix npm generated -->
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <script src="{{asset('js/app.js')}}"></script>
-        @stack('js_scripts')
-    </body>
+    </div>
+
+    <!-- ✅ Footer (Now Correctly Placed) -->
+    @include('layouts.footer')
+
+    <!-- Webpack Mix Assets -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    @stack('js_scripts')
+</body>
 </html>
