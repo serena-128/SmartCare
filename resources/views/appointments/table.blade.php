b7c6b4f28c400baa2a69eea3906e7ed6520b9abe
Komal <B00139870@mytudublin.ie>
2025-03-27 15:12:37 +0000

    appointments epic  fixed

--- resources/views/appointments/table.blade.php
+++ resources/views/appointments/table.blade.php
@@ -1,32 +1,42 @@
-<table class="table table-responsive" id="appointments-table">
+<table class="table table-striped">
     <thead>
-        <th>Residentid</th>
-        <th>Staffmemberid</th>
-        <th>Date</th>
-        <th>Time</th>
-        <th>Reason</th>
-        <th>Location</th>
-        <th colspan="3">Action</th>
+        <tr>
+            <th>Resident</th>
+            <th>Staff Member</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Reason</th>
+            <th>Location</th>
+            <th>Action</th>
+        </tr>
     </thead>
     <tbody>
-    @foreach($appointments as $appointment)
-        <tr>
-            <td>{!! $appointment->residentid !!}</td>
-            <td>{!! $appointment->staffmemberid !!}</td>
-            <td>{!! $appointment->date !!}</td>
-            <td>{!! $appointment->time !!}</td>
-            <td>{!! $appointment->reason !!}</td>
-            <td>{!! $appointment->location !!}</td>
-            <td>
-                {!! Form::open(['route' => ['appointments.destroy', $appointment->id], 'method' => 'delete']) !!}
-                <div class='btn-group'>
-                    <a href="{!! route('appointments.show', [$appointment->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-eye"></i></i></a>
-                    <a href="{!! route('appointments.edit', [$appointment->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-edit"></i></i></a>
-                    {!! Form::button('<i class="far fa-trash-alt"></i></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
-                </div>
-                {!! Form::close() !!}
-            </td>
-        </tr>
-    @endforeach
+        @foreach($appointments as $appointment)
+            <tr>
+                <td>
+                    {{ $appointment->resident ? $appointment->resident->firstname . ' ' . $appointment->resident->lastname : 'N/A' }}
+                </td>
+                <td>
+                    {{ $appointment->staffmember ? $appointment->staffmember->firstname . ' ' . $appointment->staffmember->lastname : 'N/A' }}
+                </td>
+                <td>{{ \Carbon\Carbon::parse($appointment->date)->format('Y-m-d') }}</td>
+                <td>{{ \Carbon\Carbon::parse($appointment->time)->format('H:i') }}</td>
+                <td>{{ $appointment->reason }}</td>
+                <td>{{ $appointment->location }}</td>
+                <td>
+                    <a href="{{ route('appointments.show', $appointment->id) }}" class="btn btn-sm btn-info" title="View">
+                        <i class="fas fa-eye"></i>
+                    </a>
+                    <a href="{{ route('appointments.edit', $appointment->id) }}" class="btn btn-sm btn-warning" title="Edit">
+                        <i class="fas fa-edit"></i>
+                    </a>
+                    {!! Form::open(['route' => ['appointments.destroy', $appointment->id], 'method' => 'delete', 'style' => 'display:inline']) !!}
+                        <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure you want to delete this appointment?')">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                    {!! Form::close() !!}
+                </td>
+            </tr>
+        @endforeach
     </tbody>
 </table>
