5eaab2a33d1fbdc715b038402a7ebeeb46b078bd
b00145951 <124595240+b00145951@users.noreply.github.com>
2025-03-27 21:04:33 +0000

    ALL FORMS ADD INTO DATABASE

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -2,12 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
-    use HasFactory;
-
-    protected $fillable = ['path'];
+    protected $fillable = ['filename'];
 }
