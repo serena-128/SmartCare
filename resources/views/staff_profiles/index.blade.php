c2b98a6149b3c1355fbfbfc0d0532efca01f332c
Komal <B00139870@mytudublin.ie>
2025-04-07 19:01:28 +0100

    staff_profile fix

--- resources/views/staff_profiles/index.blade.php
+++ resources/views/staff_profiles/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <section class="content-header">
-        <h1 class="pull-left">staff_profiles</h1>
+        <h1 class="pull-left">My profile </h1>
         <h1 class="pull-right">
            <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('staffProfiles.create') !!}">Add New</a>
         </h1>
@@ -15,7 +15,8 @@
         <div class="clearfix"></div>
         <div class="box box-primary">
             <div class="box-body">
-                    @include('staffProfiles.table')
+                    @include('staff_profiles.table')
+
             </div>
         </div>
     </div>
