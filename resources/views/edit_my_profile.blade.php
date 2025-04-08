37ed68edbe3e5eb20c0389816e30dc30e0300db1
Komal <B00139870@mytudublin.ie>
2025-04-08 20:31:23 +0100

    staff profile made

--- resources/views/edit_my_profile.blade.php
+++ resources/views/edit_my_profile.blade.php
@@ -11,19 +11,28 @@
     <form method="POST" action="{{ route('my.profile.update') }}" enctype="multipart/form-data">
         @csrf
 
+        <!-- Phone Number -->
         <div class="mb-3">
-            <label>Phone Number</label>
+            <label for="contactnumber">Phone Number</label>
             <input type="text" name="contactnumber" value="{{ $staff->contactnumber }}" class="form-control">
         </div>
 
+        <!-- Address -->
         <div class="mb-3">
-            <label>Profile Picture</label><br>
+            <label for="address">Address</label>
+            <input type="text" name="address" value="{{ $staff->address }}" class="form-control">
+        </div>
+
+        <!-- Profile Picture -->
+        <div class="mb-3">
+            <label for="profile_picture">Profile Picture</label><br>
             @if($staff->profile_picture)
-                <img src="{{ asset('storage/' . $staff->profile_picture) }}" width="100" class="mb-2">
+                <img src="{{ asset('storage/' . $staff->profile_picture) }}" width="100" class="mb-2 rounded">
             @endif
             <input type="file" name="profile_picture" class="form-control">
         </div>
 
+        <!-- Submit -->
         <button type="submit" class="btn btn-primary">Update Profile</button>
     </form>
 </div>
