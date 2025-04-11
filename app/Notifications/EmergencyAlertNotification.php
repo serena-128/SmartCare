cbfc9b520a417e6f1eb5869ab63284a5980af7f0
Komal <B00139870@mytudublin.ie>
2025-04-11 01:09:24 +0100

    notifications created

--- app/Notifications/EmergencyAlertNotification.php
+++ app/Notifications/EmergencyAlertNotification.php
@@ -3,59 +3,52 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\DatabaseMessage;
+use App\Models\EmergencyAlert;
 
 class EmergencyAlertNotification extends Notification
 {
     use Queueable;
 
+    protected $alert;
+
     /**
      * Create a new notification instance.
      *
-     * @return void
+     * @param \App\Models\EmergencyAlert $alert
      */
-    public function __construct()
+    public function __construct(EmergencyAlert $alert)
     {
-        //
+        $this->alert = $alert;
     }
 
     /**
      * Get the notification's delivery channels.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
      * @return array
      */
     public function via($notifiable)
     {
-        return ['mail'];
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+        return ['database']; // switch from mail to database
     }
 
     /**
      * Get the array representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
      * @return array
      */
-    public function toArray($notifiable)
+    public function toDatabase($notifiable)
     {
         return [
-            //
+            'alert_id'     => $this->alert->id,
+            'resident'     => $this->alert->resident->firstname ?? 'Unknown',
+            'type'         => $this->alert->alerttype,
+            'urgency'      => $this->alert->urgency,
+            'details'      => $this->alert->details,
+            'timestamp'    => $this->alert->alerttimestamp,
         ];
     }
 }
