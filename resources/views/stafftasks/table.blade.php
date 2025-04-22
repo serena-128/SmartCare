43af926a467c0cc33a363b4f2a91e2e0848d978a
b00145951 <124595240+b00145951@users.noreply.github.com>
2025-04-22 16:56:02 +0100

    staff tasks

--- resources/views/stafftasks/table.blade.php
+++ resources/views/stafftasks/table.blade.php
@@ -1,30 +1,30 @@
 <table class="table table-responsive" id="stafftasks-table">
     <thead>
-        <th>Staffmemberid</th>
-        <th>Taskid</th>
-        <th>Roleintask</th>
-        <th>Startdate</th>
-        <th>Enddate</th>
-        <th colspan="3">Action</th>
+        <tr>
+            <th>Staff Member</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Description</th>
+            <th colspan="3">Action</th>
+        </tr>
     </thead>
     <tbody>
-    @foreach($stafftasks as $stafftask)
-        <tr>
-            <td>{!! $stafftask->staffmemberid !!}</td>
-            <td>{!! $stafftask->taskid !!}</td>
-            <td>{!! $stafftask->roleintask !!}</td>
-            <td>{!! $stafftask->startdate !!}</td>
-            <td>{!! $stafftask->enddate !!}</td>
-            <td>
-                {!! Form::open(['route' => ['stafftasks.destroy', $stafftask->id], 'method' => 'delete']) !!}
-                <div class='btn-group'>
-                    <a href="{!! route('stafftasks.show', [$stafftask->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-eye"></i></i></a>
-                    <a href="{!! route('stafftasks.edit', [$stafftask->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-edit"></i></i></a>
-                    {!! Form::button('<i class="far fa-trash-alt"></i></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
-                </div>
-                {!! Form::close() !!}
-            </td>
-        </tr>
-    @endforeach
+        @foreach($stafftasks as $stafftask)
+            <tr>
+                <td>{{ $stafftask->staff->firstname }} {{ $stafftask->staff->lastname }}</td>
+                <td>{{ $stafftask->date }}</td>
+                <td>{{ $stafftask->time }}</td>
+                <td>{{ $stafftask->description }}</td>
+                <td>
+                    {!! Form::open(['route' => ['stafftasks.destroy', $stafftask->id], 'method' => 'delete']) !!}
+                    <div class='btn-group'>
+                        <a href="{{ route('stafftasks.show', $stafftask->id) }}" class='btn btn-default btn-xs'><i class="far fa-eye"></i></a>
+                        <a href="{{ route('stafftasks.edit', $stafftask->id) }}" class='btn btn-default btn-xs'><i class="far fa-edit"></i></a>
+                        {!! Form::button('<i class="far fa-trash-alt"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                    </div>
+                    {!! Form::close() !!}
+                </td>
+            </tr>
+        @endforeach
     </tbody>
 </table>
